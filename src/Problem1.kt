1e0e8b9b34ab991a88f231e5e8a2afc81fc90225
João Bini <joaopbini@gmail.com>
2020-01-06 13:51:17 -0300

    problem 1

---
+++ src/Problem1.kt
@@ -0,0 +1,26 @@
+/**
+ * Multiples of 3 and 5
+ * Problem 1
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+
+fun main() {
+    println(multiplesOf3and5(1000))
+}
+
+fun multiplesOf3and5(n: Int): Int {
+
+    var sum = 0
+
+    for (i in 1 until n) {
+
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i
+        }
+
+    }
+
+    return sum
+}
